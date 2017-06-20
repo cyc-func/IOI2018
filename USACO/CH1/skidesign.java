9d9f47c13b3c830fb44113c35590ba3658037932
cyc-func <yasharyang@gmail.com>
2017-06-20 07:39:40 +0530

    Update skidesign.java

--- USACO/CH1/skidesign.java
+++ USACO/CH1/skidesign.java
@@ -1,7 +1,7 @@
 /*
-ID: beatsbysuprimoforallofmypeoplenegroesandlatinoes
+ID: yashary1
 LANG: JAVA
-TASK: skidesign
+TASK: beatsbysuprimoforallofmypeoplenegrosandlatinoes
 */
 
 import java.io.*;
@@ -13,26 +13,26 @@ class skidesign
 	public static void main(String args[]) throws IOException
 	{
 		BufferedReader f = new BufferedReader(new FileReader("skidesign.in"));
-    PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("skidesign.out")));
+		PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("skidesign.out")));
 
-    int N = Integer.parseInt((new StringTokenizer(f.readLine())).nextToken());
+		int N = Integer.parseInt((new StringTokenizer(f.readLine())).nextToken());
 
-    int elevation[] = new int[1000];
-    for(int i = 0; i < N; i++)
-    {
-    	elevation[i] = Integer.parseInt((new StringTokenizer(f.readLine())).nextToken());
-    }
+		int elevation[] = new int[1000];
+		for(int i = 0; i < N; i++)
+		{
+			elevation[i] = Integer.parseInt((new StringTokenizer(f.readLine())).nextToken());
+		}
 
-    int minCost = 1000000;
-    for(int i = 1; i <= 100; i++)
-    {
-    	int currentCost = cost(i, elevation, N);
-    	if(currentCost < minCost) minCost = currentCost;
-    }
+		int minCost = 1000000;
+		for(int i = 1; i <= 100; i++)
+		{
+			int currentCost = cost(i, elevation, N);
+			if(currentCost < minCost) minCost = currentCost;
+		}
 
-    out.println(minCost);
+		out.println(minCost);
 
-    out.close();
+		out.close();
 	}
 
 	private static int cost(int minHeight, int elevation[], int N)
